27ea12ea50aa28324e690613294cc4d7e64dad23
Carsten Ringe <carsten.ringe@web.de>
2012-01-30 13:58:15 +0100

    Add partial to render directory with posts

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ exports.index = function(req, res){
   if(req.session.firstAccess == null || req.session.firstAccess == "") {
     req.session.firstAccess = new Date()
   }
-  res.render('index', { title: 'Hello World', date: new Date(), user: 'anonymous', firstAccess: req.session.firstAccess, sd: JSON.stringify(req.session) })
+  res.render('index', { title: 'Hello World', date: new Date(), user: 'anonymous', firstAccess: req.session.firstAccess, dir: 'blog' });
 };
+
