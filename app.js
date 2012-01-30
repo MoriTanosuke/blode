0382a2a7a74d951167116e62e7048ad083371e19
Carsten Ringe <carsten.ringe@web.de>
2012-01-30 13:33:08 +0100

    Add session handling

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.cookieParser());
+  app.use(express.session({secret: "keyboard cat"}));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Hello World', date: new Date(), user: 'anonymous' })
+  if(req.session.firstAccess == null || req.session.firstAccess == "") {
+    req.session.firstAccess = new Date()
+  }
+  res.render('index', { title: 'Hello World', date: new Date(), user: 'anonymous', firstAccess: req.session.firstAccess, sd: JSON.stringify(req.session) })
 };
